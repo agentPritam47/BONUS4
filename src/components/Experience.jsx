e297dd00fd3a6d332defbdb93c639578d89a9287
Pritam <pritamdas5370@gmail.com>
2024-10-25 20:30:01 +0530

    6th

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,17 +8,20 @@ const Experience = ({click}) => {
   const { scrollYProgress } = useScroll();
   const scl = Math.max(window.innerWidth / 7500, 0.15);
   let pos = 0
+  let rot = 2
   if (window.innerWidth > 900){
     pos = 0
+    rot = 2
   } else {
     pos = -0.1
+    rot = 1.9
   }
   useFrame(() => {
     const scrollValue = scrollYProgress.get(); 
     // console.log(scrollValue);
 
     watch.current.rotation.x = Math.PI / 2 - scrollValue * Math.PI/3;
-    watch.current.rotation.y = Math.PI*2  - scrollValue * Math.PI/2;
+    watch.current.rotation.y = Math.PI*2  - scrollValue * Math.PI/rot;
     watch.current.position.y = pos - scrollValue *.1 ;
   });
 
